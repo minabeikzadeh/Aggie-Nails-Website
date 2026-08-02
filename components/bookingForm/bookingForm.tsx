0334ff579b9efea01b3d995d464d7af00cbb20c7
minabeikzadeh <mina.beikzadeh.loza@gmail.com>
2026-08-02 16:21:50 -0600

    Services page completed

--- components/bookingForm/bookingForm.tsx
+++ components/bookingForm/bookingForm.tsx
@@ -6,29 +6,49 @@ export default function BookingForm(){
         <>
         <section className={styles.bookingPage}>
         <div className={styles.bookingContainer}>
-            <h1 className= {styles.title}>CHOOSE YOUR SERVICE</h1>
+
+
+            <h1 className= {styles.title}>Select a Service</h1>
+
+
             <section className={styles.serviceGrid}>
+
                 
                 <div className={styles.serviceCard}>
+
                     <Image className={styles.essential_img}
                         src= "/images/IMG_4933.jpg"
                         alt= "Essential Nails"
                         width={300}
                         height={400}
                     /> 
-                    <h3>ESSENTIAL</h3>
-                    <p>solid color | french tip | chrome</p>
+
+                    <div className={styles.serviceInfo}>
+                        <h2>Essential</h2>
+                        <ul><li>Solid color</li><li>French tip</li><li>Chrome</li></ul>
+                        <h3 className={styles.price}>$75</h3>
+                    </div>
+                    
                 </div>
 
+
                 <div className={styles.serviceCard}>
+
                     <Image className={styles.signature_img}
                         src= "/images/fairy_green_nails_edited.png"
                         alt= "Signature Nails"
                         width={300}
                         height={400}
                     /> 
-                    <h3>SIGNATURE</h3>
+
+                    <div className={styles.serviceInfo}>
+                        <h2>Signature</h2>
+                        <ul><li>3D Art</li><li>Charms and Gems</li><li>Simple Nail Art</li></ul>
+                        <h3 className={styles.price}>$95</h3>
+                    </div>
+
                 </div>
+
 
                 <div className={styles.serviceCard}>
                     <Image className={styles.statement_img}
@@ -37,11 +57,19 @@ export default function BookingForm(){
                         width={300}
                         height={400}
                     /> 
-                    <h3>STATEMENT</h3>
+
+                    <div className={styles.serviceInfo}>
+                        <h2>Statement</h2>
+                        <ul><li>Advanced Nail Art</li><li>Complex 3D Sculpting </li><li>Fully Customized Designs</li></ul>
+                        <p>Custom quote through DM</p>
+                    </div>
+
                 </div>
                 
 
             </section>
+
+
         </div>
         </section>
         </>
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 export default function NavBar(){
     return(
         <nav className={styles.navbar}>
-            <h1>Aggie Nails</h1>
+            <h1 className={styles.boldedElements}>Aggie Nails</h1>
             <div className={styles.links}>
                 <a href="/">Home</a>
                 <a href="/gallery">Gallery</a>
                 <a href="/about">About</a>
                 <a href="/faq">FAQ</a>
             </div>
-            <Link href="/book">
-                <button>BOOK NOW</button>
+            <Link href="/book" >
+                <button className={styles.boldedElements} >Book Now</button>
             </Link>
 
         </nav>
